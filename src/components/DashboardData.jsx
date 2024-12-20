ed603703cd76599d78f2b1095e000424a916951d
Naveen Sharma <98882301+NaveenWebDev@users.noreply.github.com>
2024-12-20 19:31:04 +0530

    change card content

--- src/components/DashboardData.jsx
+++ src/components/DashboardData.jsx
@@ -44,14 +44,14 @@ const DashboardData = () => {
                 </div>
                 <div className="bottom border-top-black-10 row p-2">
                   <span className="text-success-100 col-12"> Total Budget</span>
-                  <span className="text-black"> 2,000,000.0 </span>
+                  <span className="text-black fw-600"> 2,000,000.0 </span>
                 </div>
               </div>
             </div>
             <div className="col-4 p-2">
               <div className="d-flex rounded-2 flex-column bg-white justify-content-between h-100">
                 <div className="bg-black-10 border-radius-t8px py-2 px-3 d-flex justify-content-between align-items-center ">
-                  <span>Scheme Budget </span>
+                  <span>Utilized Bidget </span>
                   <span>
                     <i className="icon-open_in_new"></i>
                   </span>
@@ -66,14 +66,14 @@ const DashboardData = () => {
                 </div>
                 <div className="bottom border-top-black-10 row p-2">
                   <span className="text-success-100 col-12"> Total Budget</span>
-                  <span className="text-black"> 2,000,000.0 </span>
+                  <span className="text-black fw-600"> 2,000,000.0 </span>
                 </div>
               </div>
             </div>
             <div className="col-4 p-2">
               <div className="d-flex rounded-2 flex-column bg-white justify-content-between h-100">
                 <div className="bg-black-10 border-radius-t8px py-2 px-3 d-flex justify-content-between align-items-center">
-                  <span>Scheme Budget </span>
+                  <span>Scheme Balance</span>
                   <span>
                     <i className="icon-open_in_new"></i>
                   </span>
@@ -88,14 +88,14 @@ const DashboardData = () => {
                 </div>
                 <div className="bottom border-top-black-10 row p-2">
                   <span className="text-success-100 col-12"> Total Budget</span>
-                  <span className="text-black"> 2,000,000.0 </span>
+                  <span className="text-black fw-600"> 2,000,000.0 </span>
                 </div>
               </div>
             </div>
             <div className="col-4 p-2">
               <div className="d-flex rounded-2 flex-column bg-white justify-content-between h-100">
                 <div className="border-radius-t8px py-2 px-3 bg-black-10 d-flex justify-content-between align-items-center">
-                  <span>Scheme Budget </span>
+                  <span>Approvals </span>
                   <span>
                     <i className="icon-open_in_new"></i>
                   </span>
@@ -110,14 +110,14 @@ const DashboardData = () => {
                 </div>
                 <div className="bottom border-top-black-10 row p-2">
                   <span className="text-success-100 col-12"> Total Budget</span>
-                  <span className="text-black"> 2,000,000.0 </span>
+                  <span className="text-black fw-600"> 2,000,000.0 </span>
                 </div>
               </div>
             </div>
             <div className="col-4 p-2">
               <div className="d-flex rounded-2 flex-column bg-white justify-content-between h-100">
                 <div className="bg-black-10 border-radius-t8px py-2 px-3 d-flex justify-content-between align-items-center">
-                  <span>Scheme Budget </span>
+                  <span>Budget Utilized Dec24 </span>
                   <span>
                     <i className="icon-open_in_new"></i>
                   </span>
@@ -132,29 +132,29 @@ const DashboardData = () => {
                 </div>
                 <div className="bottom border-top-black-10 row p-2">
                   <span className="text-success-100 col-12"> Total Budget</span>
-                  <span className="text-black"> 2,000,000.0 </span>
+                  <span className="text-black fw-600"> 2,000,000.0 </span>
                 </div>
               </div>
             </div>
             <div className="col-4 p-2">
               <div className="d-flex rounded-2 flex-column bg-white justify-content-between h-100">
-                <div className="bg-black-10 border-radius-t8px py-2 px-3 d-flex justify-content-between a;ogm-items-center">
-                  <span>Scheme Budget </span>
+                <div className="bg-black-10 border-radius-t8px py-2 px-3 d-flex justify-content-between align-items-center">
+                  <span>Accumulated Deduction </span>
                   <span>
                     <i className="icon-open_in_new"></i>
                   </span>
                 </div>
                 <div className="mid p-2">
-                  <p className="text-black-60 fs-12px">Provisioned Budget</p>
-                  <span className="fw-600">0</span>
-                  <p className="text-black-60 fs-12px">Released Budget</p>
-                  <span className="fw-600">2,000,000.00</span>
-                  <p className="text-black-60 fs-12px">Prev. Year Balance</p>
-                  <span className="fw-600">2,0</span>
+                  <p className="text-black-60 fs-12px">From Current FV</p>
+                  <span className="fw-600">0.00</span>
+                  <p className="text-black-60 fs-12px"></p>
+                  <span className="fw-600"></span>
+                  <p className="text-black-60 fs-12px"></p>
+                  <span className="fw-600"></span>
                 </div>
                 <div className="bottom border-top-black-10 row p-2">
                   <span className="text-success-100 col-12"> Total Budget</span>
-                  <span className="text-black"> 2,000,000.0 </span>
+                  <span className="text-black fw-600"> 2,000,000.0 </span>
                 </div>
               </div>
             </div>
@@ -180,7 +180,7 @@ const DashboardData = () => {
             <div className="col-6 p-2">
               <div className="d-flex rounded-2 flex-column bg-white justify-content-between h-100">
                 <div className="border-radius-t8px py-2 px-3 bg-black-10 d-flex justify-content-between align-items-center">
-                  <span>Graph 1 </span>
+                  <span>Graph 2 </span>
                   <span>
                     <i className="icon-open_in_new"></i>
                   </span>
@@ -210,9 +210,9 @@ const DashboardData = () => {
                 <span className="fw-600">State Sarra Center</span>
 
                 <div className="row">
-                  <div className="col-6 p-1">
-                    <div className="d-flex justify-content-between  align-items-center ">
-                      <p className="text-black-60">Account No</p>
+                  <div className="col-6 p-1 bg-prime-20-hover cursor-pointer">
+                    <div className="d-flex justify-content-between align-items-center ">
+                      <p className="text-black-60 fs-12px">Account No</p>
                       <i className="icon-open_in_new"></i>
                     </div>
                     <span className="fw-600">50100590094480</span>
@@ -220,7 +220,7 @@ const DashboardData = () => {
 
                   <div className="col-6 p-1">
                     <div className="d-flex justify-content-between  align-items-center">
-                      <p className="text-black-60">Account Balance</p>
+                      <p className="text-black-60 fs-12px">Account Balance</p>
                       {/* <i className="icon-open_in_new"></i> */}
                     </div>
                     <span className="fw-600">50100590094480</span>
@@ -228,7 +228,7 @@ const DashboardData = () => {
 
                   <div className="col-6 p-1">
                     <div className="d-flex justify-content-between  align-items-center">
-                      <p className="text-black-60">Balance As On </p>
+                      <p className="text-black-60 fs-12px">Balance As On </p>
                       {/* <i className="icon-open_in_new"></i> */}
                     </div>
                     <span className="fw-600">2024-08-31</span>
@@ -236,7 +236,7 @@ const DashboardData = () => {
 
                   <div className="col-6 p-1">
                     <div className="d-flex justify-content-between  align-items-center">
-                      <p className="text-black-60">Account Interest</p>
+                      <p className="text-black-60 fs-12px">Account Interest</p>
                       {/* <i className="icon-open_in_new"></i> */}
                     </div>
                     <span className="fw-600">0</span>
@@ -244,14 +244,14 @@ const DashboardData = () => {
 
                   <div className="col-6 p-1">
                     <div className="d-flex justify-content-between  align-items-center">
-                      <p className="text-black-60">Interest Date</p>
+                      <p className="text-black-60 fs-12px">Interest Date</p>
                       {/* <i className="icon-open_in_new"></i> */}
                     </div>
                     <span className="fw-600">2024-06-03</span>
                   </div>
                   <div className="col-6 p-1">
                     <div className="d-flex justify-content-between  align-items-center">
-                      <p className="text-black-60">Quarter</p>
+                      <p className="text-black-60 fs-12px">Quarter</p>
                       {/* <i className="icon-open_in_new"></i> */}
                     </div>
                     <span className="fw-600">1</span>
@@ -267,7 +267,7 @@ const DashboardData = () => {
                   </p>
                   <span className="text-black fw-500"> Approved</span>
                   </span>
-                  <i className="icon-verified "></i>
+                  <i className="icon-verified text-success-100"></i>
                 </div>
                 <div className="bottom border-top-black-10 col-6 p-2 bg-warning-20 d-flex justify-content-between align-items-center">
                 <span>
